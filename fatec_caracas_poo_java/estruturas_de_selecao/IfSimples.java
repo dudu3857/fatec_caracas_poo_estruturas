4147cad546e0f2b9905cf87bb3c2833be69c3b55
Eduardo Francisco <efsgames57@gmail.com>
2021-03-03 09:46:02 -0300

    if simples

--- fatec_caracas_poo_java/estruturas_de_selecao/IfSimples.java
+++ fatec_caracas_poo_java/estruturas_de_selecao/IfSimples.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 public class IfSimples{
-   public static void public static void main(String[] args) {
+    public static void main(String[] args) {
        double nota;
        nota =Double.parseDouble(JOptionPane.showInputDialog ("Digite a nota"));
        /*esse é um comentario de varias
