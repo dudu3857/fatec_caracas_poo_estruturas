d8557a6f57ad43d7e415589545287fba295e28ca
Eduardo Francisco <efsgames57@gmail.com>
2021-03-03 10:02:49 -0300

    if else

---
+++ fatec_caracas_poo_java/estruturas_de_selecao/IfElse.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+public class IfElse {
+    public static void main(String[] args) {
+         float nota;
+         nota = Float.parseFloat(JOptionPane.showInputDialog("Digite a nota"));
+         if (nota >= 70){
+            JOptionPane.showMessageDialog(null, "Aprovado");
+            JOptionPane.showMessageDialog(null, "Show");
+         }
+         
+         else{
+            JOptionPane.showMessageDialog(null, "Reprovado");
+         }
+         
+    }
+}
